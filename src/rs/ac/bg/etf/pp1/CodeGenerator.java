778c55e48d8f7fed0ecbd61d277aa6fea0cb3314
Fedja Mladenovic <fedjamladenovic@gmail.com>
2023-06-23 18:45:56 +0200

    alociranje matrice
    i popravljena greška enter potpisu funkcija

--- src/rs/ac/bg/etf/pp1/CodeGenerator.java
+++ src/rs/ac/bg/etf/pp1/CodeGenerator.java
@@ -65,7 +65,8 @@ public class CodeGenerator extends VisitorAdaptor {
 		}
 		method.obj.setAdr(Code.pc);
 		Code.put(Code.enter);
-		Code.put(method.obj.getLevel());
+
+		Code.put(sem.allFunctions.get(method.obj.getName()).parCount);
 		Code.put(method.obj.getLocalSymbols().size());
 	}
 
@@ -223,9 +224,99 @@ public class CodeGenerator extends VisitorAdaptor {
 		Code.put(factor.struct.equals(Tab.charType) ? 0 : 1);
 	}
 
+	public void swapTop() {
+		Code.put(Code.dup_x1);
+		Code.put(Code.pop);
+	}
+
 	public void visit(FactorNewMatrix factor) {
-		// TODO
+		boolean charType = factor.struct.equals(Tab.charType);
+
+		// mat[e1][e2], e1 je broj vrsta a e2 broj kolona   STEK
+		//													..., e1, e2
+		swapTop(); //										..., e2, e1
+		Code.put(Code.dup); //                              ..., e2, e1, e1
+		Code.put(Code.newarray);
+		Code.put(1); //										..., e2, e1, mat				mat = new array[e1];
+		swapTop(); //										..., e2, mat, e1
+		Code.put(Code.const_1); //							..., e2, mat, e1, 1
+		Code.put(Code.sub); //								..., e2, mat, e1				e1--;
+
+		// FOR:
+		int forPc = Code.pc; // 						FOR:
+		Code.put(Code.dup); //								..., e2, mat, e1, e1
+		Code.loadConst(0); //								..., e2, mat, e1, e1, 0
+		Code.putFalseJump(Code.ge, 0); //					..., e2, mat, e1				if (0 >= e1) jmp END;
+		int falseJump = Code.pc-2;
+
+		Code.put(Code.dup_x2); //							..., e1, e2, mat, e1
+		Code.put(Code.dup_x2); //							..., e1, e1, e2, mat, e1
+		Code.put(Code.pop); //								..., e1, e1, e2, mat
+		Code.put(Code.dup_x2); //							..., e1, mat, e1, e2, mat
+		Code.put(Code.dup_x2); //							..., e1, mat, mat, e1, e2, mat
+		Code.put(Code.pop); //								..., e1, mat, mat, e1, e2
+		Code.put(Code.newarray);
+		Code.put(charType ? 0 : 1); //						..., e1, mat, mat, e1, arr		arr = new array[e2];
+		Code.put(Code.dup_x2); //							..., e1, mat, arr, mat, e1, arr
+		Code.put(charType ? Code.bastore : Code.astore); // ..., e1, mat, arr				mat[e1] = arr;
+		Code.put(Code.arraylength); //						..., e1, mat, e2
+
+		Code.put(Code.dup_x2); //							..., e2, e1, mat, e2
+		Code.put(Code.pop); //								..., e2, e1, mat
+		Code.put(Code.dup_x1); //							..., e2, mat, e1, mat
+		Code.put(Code.pop); //								..., e2, mat, e1
+
+		Code.put(Code.const_1); //							..., e2, mat, e1, 1
+		Code.put(Code.sub); //								..., e2, mat, e1				e1--;
+		Code.putJump(forPc); //								..., e2, mat, e1				jmp FOR
+		Code.fixup(falseJump);
+
+		// END:											END:
+		Code.put(Code.pop); //								..., e2, mat
+		swapTop(); //										..., mat, e2
+		Code.put(Code.pop); //								..., mat
+
+		/*
+		  [START]	  ..., e1, e2
+		  swap        ..., e2, e1
+		  dup         ..., e2, e1, e1
+		  newarray 1  ..., e2, e1, mat
+		  swap        ..., e2, mat, e1
+		  const_1     ..., e2, mat, e1, 1
+		  sub         ..., e2, mat, e1=e1-1
+for:
+		  dup         ..., e2, mat, e1, e1
+		  const_0     ..., e2, mat, e1, e1, 0
+		  jlt [END]   ..., e2, mat, e1
+
+		  dup_x2      ..., e1, e2, mat, e1
+		  dup_x2      ..., e1, e1, e2, mat, e1
+		  pop         ..., e1, e1, e2, mat
+		  dup_x2      ..., e1, mat, e1, e2, mat
+		  dup_x2      ..., e1, mat, mat, e1, e2, mat
+		  pop         ..., e1, mat, mat, e1, e2
+		  newarray ?  ..., e1, mat, mat, e1, arr
+		  dup_x2      ..., e1, mat, arr, mat, e1, arr
+		  bastore     ..., e1, mat, arr
+		  arraylen    ..., e1, mat, e2
+
+		  dup_x2      ..., e2, e1, mat, e2
+		  pop         ..., e2, e1, mat
+		  dup_x1      ..., e2, mat, e1, mat
+		  pop         ..., e2, mat, e1
+
+		  const_1     ..., e2, mat, e1, 1
+		  sub         ..., e2, mat, e1=e1-1
+		  jmp [FOR]   ..., e2, mat, e1
+end:
+		  pop         ..., e2, mat
+		  swap        ..., mat, e2
+		  pop         ..., mat
+		 */
 	}
 
 	public void visit(FactorExpr factor) { /* empty: Expr se propagira na gore */ }
+
+	// ------------------------------------ Designator --------------------------------------------
+
 }
